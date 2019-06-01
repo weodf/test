a914383f1bb2672a3697318a0c3a09a9a5b19dcd
asdsunny <395359511@qq.com>
2019-06-01 10:44:07 +0800

    test

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+//	System.out.println("hellow world");
+//	System.out.println(queue);
+	public static void main(String[] args){
+		System.out.println("hello world");
+		System.out.println("hehe");
+
+        
+    }
+
+}
