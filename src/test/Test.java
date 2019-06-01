3cf12f175a4b5f46071ea7b687f8857207c908b8
asdsunny <395359511@qq.com>
2019-06-01 10:48:25 +0800

    添加注释

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,11 @@
 package test;
 
 public class Test {
-//	System.out.println("hellow world");
-//	System.out.println(queue);
+
+	/**
+	 * ×¢ÊÍ
+	 * @param args
+	 */
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("hehe");
