f9bb7916bfc26ddfdbd40bd8aab407c0d89274b5
asdsunny <395359511@qq.com>
2019-06-01 12:17:49 +0800

    delete

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,12 +7,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args){
-		System.out.println("hello world");
-		System.out.println("hehe");
-		System.out.println("you");
-		System.out.println("zuo");
-		System.out.println("test");
-		System.out.println("you");
+		
     }
 
 }
